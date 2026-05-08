b502be5f5fc252cbe4a08fb32239e67e03fb1ab2
Rostyslav <rooostik2016@gmail.com>
2026-05-09 00:05:35 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,18 @@
 'use strict';
 
-const inputs = [...document.querySelectorAll('input')];
+const inputs = [...document.querySelector('form').querySelectorAll('input')];
 
 inputs.forEach((e) => {
   const label = document.createElement('label');
 
   label.classList.add('field-label');
   label.setAttribute('htmlFor', `${e.id}`);
-  label.textContent = e.name.charAt(0).toUpperCase() + e.name.slice(1);
 
-  e.setAttribute('placeholder', `${e.name}`);
+  const capitalizedName = e.name.charAt(0).toUpperCase() + e.name.slice(1);
+
+  label.textContent = capitalizedName;
+
+  e.setAttribute('placeholder', `${capitalizedName}`);
 
   e.before(label);
 });
